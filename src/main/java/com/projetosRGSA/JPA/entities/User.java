8e236d704ff99b7bca1101ae64a47ba0ec8aa066
rodneigstv <rodneigstv@hotmail.com>
2023-01-26 20:53:25 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projetosRGSA/JPA/entities/User.java
+++ src/main/java/com/projetosRGSA/JPA/entities/User.java
@@ -3,12 +3,22 @@ package com.projetosRGSA.JPA.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String name;
 	private String email;
 	private String phone;
@@ -83,5 +93,6 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+	
 
 }
